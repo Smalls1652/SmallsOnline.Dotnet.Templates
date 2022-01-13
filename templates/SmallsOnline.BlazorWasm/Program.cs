335c691a3693d0afaf45990c24e2b51ca0357428
Timothy Small <smalls.online@icloud.com>
2022-01-13 11:24:24 -0500

    Completely overhauled the BlazorWASM template

--- templates/SmallsOnline.BlazorWasm/Program.cs
+++ templates/SmallsOnline.BlazorWasm/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Http;
+
 using SmallsOnline.BlazorWasm.Template;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(
+    sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }
+);
 
 await builder.Build().RunAsync();
